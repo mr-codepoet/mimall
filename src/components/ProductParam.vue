e93d1fb6915f095015d9aaa857bd6c4e450155d2
pengkai <pengkaie_mail@sina.com>
2021-02-08 20:32:19 +0800

    第一次提交

---
+++ src/components/ProductParam.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="nav-bar" :class="{ is_fixed: isFixed }">
+    <div class="container">
+      <div class="pro-title">
+        {{ title }}
+      </div>
+      <div class="pro-param">
+        <a href="javascript:;">概述</a><span>|</span>
+        <a href="javascript:;">参数</a><span>|</span>
+        <a href="javascript:;">用户评价</a>
+        <slot name="buy"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import { onMounted, onUnmounted, ref } from "vue";
+export default {
+  name: "nav-bar",
+  props: {
+    title: String,
+  },
+  setup() {
+    const isFixed = ref(false);
+    onMounted(() => {
+      window.addEventListener("scroll", initHeight);
+    });
+    onUnmounted(() => {
+      window.removeEventListener("scroll", initHeight, false);
+    });
+    function initHeight() {
+      let scrollTop =
+        window.pageYOffset ||
+        document.documentElement.scrollTop ||
+        document.body.scrollTop;
+      isFixed.value = scrollTop > 152 ? true : false;
+    }
+    return {
+      isFixed,
+      initHeight,
+    };
+  },
+};
+</script>
+<style lang="scss">
+@import "./../assets/scss/config.scss";
+@import "./../assets/scss/mixin.scss";
+.nav-bar {
+  height: 70px;
+  line-height: 70px;
+  border-top: 1px solid $colorH;
+  background-color: $colorG;
+  z-index: 10;
+  &.is_fixed {
+    position: fixed;
+    top: 0;
+    width: 100%;
+    box-shadow: 0 5px 5px $colorE;
+  }
+  .container {
+    @include flex();
+    .pro-title {
+      font-size: $fontH;
+      color: $colorB;
+      font-weight: bold;
+    }
+    .pro-param {
+      font-size: $fontJ;
+      span {
+        margin: 0 10px;
+      }
+      a {
+        color: $colorC;
+      }
+    }
+  }
+}
+</style>
